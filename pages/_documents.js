ad838d33edc41ab6f39c828dc0a9220d611f60af
Akshara Mishra <akshara20@navgurukul.org>
2022-08-09 17:05:39 +0530

    responsive webpage

--- pages/_documents.js
+++ pages/_documents.js
@@ -1,14 +1,18 @@
+import Document, { Html, Head } from "next/document";
+
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&family=Rubik:ital,wght@0,400;1,300;1,400&display=swap" rel="stylesheet"/>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&family=Rubik:ital,wght@0,400;1,300;1,400&display=swap"
+            rel="stylesheet"
+          />
         </Head>
-        <body>
-        </body>
+        <body></body>
       </Html>
     );
   }
